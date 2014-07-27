165f0efa5c359711a3a9c2cc6d3fa66040ae8826
Vladimir.Krasikov <vladimir@krasikov.pro>
2014-07-27 23:19:39 +0400

    add application config.

---
+++ java/spring-beans-life-cycle/spring-beans-life-cycle/src/main/java/pro/krasikov/sample/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package pro.krasikov.sample.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Vladimir Krasikov (mailto: vladimir@krasikov.pro)
+ *         Date: 27.07.2014.
+ */
+@Configuration
+@ComponentScan("pro.krasikov.sample")
+public class ApplicationConfig {
+}
