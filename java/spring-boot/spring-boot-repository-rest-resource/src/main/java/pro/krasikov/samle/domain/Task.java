b9d9aafe9cb6945bfdffa6c8fe72de0c57b4fe14
Vladimir.Krasikov <vladimir@krasikov.pro>
2014-08-01 00:31:51 +0400

    add domain.

---
+++ java/spring-boot/spring-boot-repository-rest-resource/src/main/java/pro/krasikov/samle/domain/Task.java
@@ -0,0 +1,61 @@
+package pro.krasikov.samle.domain;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * @author Vladimir Krasikov (mailto: vladimir@krasikov.pro)
+ *         Date: 31.07.2014.
+ */
+@Entity
+public class Task implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    private String description;
+
+    public Task() {
+    }
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
